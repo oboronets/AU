fc15be4c38536c6f1f8fe5ecdfff6abeaaac4928
GrOboronets <103806352+GrOboronets@users.noreply.github.com>
2022-10-01 15:47:49 +0300

    Update graf.py

--- graf.py
+++ graf.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#import math
-#import numpy
-#import matplotlib.pyplot as mpp
+import math  # подключение пакета math
+import numpy  # подключение пакета numpy
+import matplotlib.pyplot as mpp  # подключение пакета Matplotlib, переименованного в mpp
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-#if __name__=='__main__':
-#    arguments = numpy.arange(0, 200, 0.1)
-#    mpp.plot(
-#        arguments,
-#        [math.sin(a) * math.sin(a/20.0) for a in arguments]
-#    )
-#    mpp.show()
+if __name__=='__main__':  # если файл запущен напрямую (не как подключенный модуль), то выполняется следущее:
+    arguments = numpy.arange(0, 200, 0.1)  # генерирует массив чисел [0, 200) с шагом 0.1
+    mpp.plot(  # строет график для x в [0, 200)...
+        arguments,
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]  # ...функции f = sin(x) * sin(x/20)
+    )
+    mpp.show()  # выводит этот график на экран
